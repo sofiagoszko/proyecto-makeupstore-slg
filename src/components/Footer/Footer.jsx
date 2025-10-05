0ce691c7382a46f7ba84f693c9d7b11c8144d270
Sofia Goszko <sofiagoszko@gmail.com>
2025-10-05 14:33:21 -0300

    cambio en los iconos al pasar el cursor por encima

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -13,13 +13,13 @@ export default function Footer(){
                     <h3>STORE</h3>
                     <ul className='list'>
                         <li>
-                            <Link as={Link} to="/" className='link'>Home</Link>
+                            <Link as={Link} to='/' className='link'>Home</Link>
                         </li>
                         <li>
-                            <Link as={Link} to="/" className='link'>Productos</Link>
+                            <Link as={Link} to='/' className='link'>Productos</Link>
                         </li>
                         <li>
-                            <Link as={Link} to="/" className='link'>Contacto</Link>
+                            <Link as={Link} to='/' className='link'>Contacto</Link>
                         </li>
                     </ul>
                 </div>
@@ -27,14 +27,14 @@ export default function Footer(){
                 <div className='footer-section'>
                     <h3>Seguinos</h3>
                     <div className='d-flex justify-content-center align-items-center'>
-                        <a href="https://www.facebook.com/" target="_blank" className='mx-2'>
-                            <FontAwesomeIcon icon={faFacebookF} size="lg" color="#fff0f3" />
+                        <a href='https://www.facebook.com/' target='_blank' className='mx-2'>
+                            <FontAwesomeIcon icon={faFacebookF} size='lg' color='#fff0f3' className='font-icon'/>
                         </a>
-                        <a href="https://www.instagram.com/" target="_blank" className='mx-2'>
-                            <FontAwesomeIcon icon={faInstagram} size="lg" color="#fff0f3" />
+                        <a href='https://www.instagram.com/' target='_blank' className='mx-2'>
+                            <FontAwesomeIcon icon={faInstagram} size='lg' color='#fff0f3' className='font-icon'/>
                         </a>
-                        <a href="https://www.tiktok.com/" target="_blank" className='mx-2'>
-                            <FontAwesomeIcon icon={faTiktok} size="lg" color="#fff0f3" />
+                        <a href='https://www.tiktok.com/' target='_blank' className='mx-2'>
+                            <FontAwesomeIcon icon={faTiktok} size='lg' color='#fff0f3' className='font-icon'/>
                         </a>
                     </div>
                 </div>
@@ -42,8 +42,8 @@ export default function Footer(){
                 
                 <div className='footer-section'>
                     <h3>Contáctanos</h3>
-                    <a href="mailto:info@sofiasmakeup.com" className='email'>
-                        <FontAwesomeIcon icon={faEnvelope} size="lg" className='mx-2' color="#fff0f3" /> 
+                    <a href='mailto:info@sofiasmakeup.com' className='email'>
+                        <FontAwesomeIcon icon={faEnvelope} size='lg' className='mx-2' color='#fff0f3' /> 
                         info@sofiasmakeup.com
                     </a>
                 </div>
