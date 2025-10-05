fda0dd5bdea5de3815ca71d14de8c6fdc3db25f0
Sofia Goszko <sofiagoszko@gmail.com>
2025-10-05 20:40:31 -0300

    cambios para hacerlo responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import './Header.css';
 export default function Header(){
     return(
         <header className='p-2'>
-            <h1 className='title-header'>SOFIA'S MAKEUP STORE</h1>
+            <h1 className='title-header mt-2'>SOFIA'S MAKEUP STORE</h1>
         </header>
     );
 }
