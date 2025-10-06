c64b6267fb4154a381b0cf605078613a78020bff
Sofia Goszko <sofiagoszko@gmail.com>
2025-10-06 11:42:37 -0300

    cambio en los estilos del carrusel para que ocupe todo el ancho se la pantalla

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,37 +6,41 @@ import './Home.css';
 
 export default function Home (){
     return(
-        <Container>
-            <CarouselHome />
-            <section className='mb-4'>
-                <div className="row text-center">
-                    <div className="col-12 col-md-4 p-2 info-home my-2">
-                        <div>
-                            <FontAwesomeIcon icon={faTruck} size='3x' className='color-icon'/>
-                        </div>
-                        <div className='info-text'>
-                            <p className="mb-2"> ENVIOS GRATIS EN COMPRAS SUPERIORES A $100.000 </p>
-                        </div>
+        <>
+            <div className="carousel-wrapper">
+                <CarouselHome />
+            </div>
+            <Container>
+                <section className='mb-4'>
+                    <div className="row text-center">
+                        <div className="col-12 col-md-4 p-2 info-home my-2">
+                            <div>
+                                <FontAwesomeIcon icon={faTruck} size='3x' className='color-icon'/>
+                            </div>
+                            <div className='info-text'>
+                                <p className="mb-2"> ENVIOS GRATIS EN COMPRAS SUPERIORES A $100.000 </p>
+                            </div>
 
+                        </div>
+                        <div className="col-12 col-md-4 p-2 border-lr info-home">
+                            <div>
+                                <FontAwesomeIcon icon={faFaceSmile} size='3x' className='color-icon' />
+                            </div>
+                            <div className='info-text'>
+                                <p className="mb-2"> 20% DE DESCUENTO EN TRANSFERENCIAS </p>
+                            </div>
+                        </div>
+                        <div className="col-12 col-md-4 p-2 info-home">
+                            <div>
+                                <FontAwesomeIcon icon={faHeart} size='3x' className='color-icon' />
+                            </div>
+                            <div className='info-text'>
+                                <p className="mb-2">PRODUCTOS A PEDIDO Y ENCARGOS PERSONALIZADOS</p>
+                            </div>
+                        </div>
                     </div>
-                    <div className="col-12 col-md-4 p-2 border-lr info-home">
-                        <div>
-                            <FontAwesomeIcon icon={faFaceSmile} size='3x' className='color-icon' />
-                        </div>
-                        <div className='info-text'>
-                            <p className="mb-2"> 20% DE DESCUENTO EN TRANSFERENCIAS </p>
-                        </div>
-                    </div>
-                    <div className="col-12 col-md-4 p-2 info-home">
-                        <div>
-                            <FontAwesomeIcon icon={faHeart} size='3x' className='color-icon' />
-                        </div>
-                        <div className='info-text'>
-                            <p className="mb-2">PRODUCTOS A PEDIDO Y ENCARGOS PERSONALIZADOS</p>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </Container>
+                </section>
+            </Container>
+        </>
     )
 };
