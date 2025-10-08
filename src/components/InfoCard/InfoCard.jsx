899cf6eb5a46e18a53a12a943b2be62391f6f72e
Sofia Goszko <sofiagoszko@gmail.com>
2025-10-08 11:09:05 -0300

    hotfix css del home

--- src/components/InfoCard/InfoCard.jsx
+++ src/components/InfoCard/InfoCard.jsx
@@ -3,7 +3,7 @@ import './InfoCard.css';
 
 export default function InfoCard ({icon, text, border}){
     return (
-        <div className={`col-12 col-md-4 p-2 info-home ${border ? 'border-lr' : ''}`}>
+        <div className={`col-12 col-md-4 p-2 ${border ? 'border-lr' : ''}`}>
             <div>
                 <FontAwesomeIcon icon={icon} size='3x' className='color-icon m-2'/>
             </div>
