5a92a22013a6ae15938e29a5dadb0cea03be5d9c
Sofia Goszko <sofiagoszko@gmail.com>
2025-10-08 09:39:21 -0300

    se paso el fetch y filtrado desde cardProducto a productos

--- src/components/CardProducto/CardProducto.jsx
+++ src/components/CardProducto/CardProducto.jsx
@@ -1,47 +1,12 @@
-import { useState, useEffect } from 'react';
-import { Button, Card, Form} from 'react-bootstrap';
+import { Button, Card} from 'react-bootstrap';
 import './CardProducto.css';
 
-export default function CardProducto() {
-    const [productos, setProductos] = useState([]);
-    const [cargando, setCargando] = useState(true);
-    const [categoria, setCategoria] = useState('todos');
-
-    useEffect(() => {
-        fetch('https://68e2cbbc8e14f4523dabd226.mockapi.io/api/v1/productos')
-        .then((res) => res.json())
-        .then((data) =>{
-        setProductos(data);
-        })
-        .catch((error) => console.error('Error al cargar productos: ', error))
-        .finally(() => setCargando(false))
-    }, []);
-
-    if(cargando) return <p>Cargando productos...</p>;
-
-    const productosFiltrados = categoria === 'todos'
-        ? productos
-        : productos.filter(producto => producto.categoria === categoria);
-
+export default function CardProducto({productos}) {
 
     return (
         <>
-            <div className="mb-4 d-flex align-items-center gap-2">
-                <label htmlFor="filtroCategoria" className="mb-2">Filtra por:</label>
-                <Form.Select 
-                    value={categoria} 
-                    onChange={(e) => setCategoria(e.target.value)} 
-                    style={{ width: '200px'}}
-                >
-                    <option value="todos">Todos</option>
-                    <option value="rostro">Rostro</option>
-                    <option value="ojos">Ojos</option>
-                    <option value="labios">Labios</option>
-                </Form.Select>
-            </div>
-
             <div className="row justify-content-center align-items-stretch g-4">
-                {productosFiltrados.map(producto => 
+                {productos.map(producto => 
                     <div key={producto.id} className="col-sm-6 col-md-4 col-lg-3 d-flex">
                         <Card className="h-100 shadow-sm w-100">
                             <Card.Img variant="top" src={producto.avatar} alt={producto.name} className='img-card'/>
--- src/pages/Productos/Productos.jsx
+++ src/pages/Productos/Productos.jsx
@@ -1,17 +1,53 @@
+import { useState, useEffect } from 'react';
 import CardProducto from "../../components/CardProducto/CardProducto";
-import { Container } from 'react-bootstrap';
+import { Container, Form } from 'react-bootstrap';
 import makeup from '../../assets/makeup.jpeg';
 import Banner from "../../components/Banner/Banner";
 import './Productos.css';
 
 export default function Productos(){
+  const [productos, setProductos] = useState([]);
+  const [cargando, setCargando] = useState(true);
+  const [categoria, setCategoria] = useState('todos');
+
+  useEffect(() => {
+      fetch('https://68e2cbbc8e14f4523dabd226.mockapi.io/api/v1/productos')
+      .then((res) => res.json())
+      .then((data) =>{
+      setProductos(data);
+      })
+      .catch((error) => console.error('Error al cargar productos: ', error))
+      .finally(() => setCargando(false))
+  }, []);
+
+  if(cargando) return <p>Cargando productos...</p>;
+
+  const productosFiltrados = categoria === 'todos'
+      ? productos
+      : productos.filter(producto => producto.categoria === categoria);
+
+
   return (
     <>
       <div className="mt-2">
         <Banner imagen={makeup} titulo="NUESTROS PRODUCTOS" />
       </div>
+
+      <div className="m-4 d-flex align-items-center gap-2">
+        <label htmlFor="filtroCategoria" className="m-2">Categoría:</label>
+        <Form.Select 
+            value={categoria} 
+            onChange={(e) => setCategoria(e.target.value)} 
+            style={{ width: '200px'}}
+        >
+            <option value="todos">Todos</option>
+            <option value="rostro">Rostro</option>
+            <option value="ojos">Ojos</option>
+            <option value="labios">Labios</option>
+        </Form.Select>
+      </div>
       <Container className="my-5">
-        <CardProducto />
+        <CardProducto productos={productosFiltrados} />
       </Container>
     </>
   );
